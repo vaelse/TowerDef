413305817b1c147fc1fb795493032ddb95698052
Maelse <niedzwiedzmaciej4@gmail.com>
2020-05-20 17:37:40 +0100

    Tower Defense
    Level done, castle and monster added as well as theirs hp bars

---
+++ Assets/Script/MeshPath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeshPath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject castle = GameObject.Find("Castle");
+        if(castle)
+        {
+            GetComponent<NavMeshAgent>().destination = castle.transform.position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject Monster;
+     int monsterCount = 0;
+    float spawnTime;
+    float respawnDelay = 2f;
+    public void Update()
+    {
+        Debug.Log(monsterCount);
+        if (monsterCount < 5)
+            spawn();      
+    }
+
+    public void spawn()
+    {
+        if (spawnTime + respawnDelay > Time.unscaledTime)
+            return;
+        spawnTime = Time.unscaledTime;
+        Instantiate(Monster, gameObject.transform);
+        monsterCount++;
+    }
+}
